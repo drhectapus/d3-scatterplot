f116c5229722256d6bc5ab0b71362f7a4cda7804
Joseph <riverfish@gmail.com>
2017-07-15 00:16:24 +0800

    First deploy

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
 
-export default class App extends Component {
+class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.createScatterplot = this.createScatterplot.bind(this);
+  //   this.props.fetchData();
+  //   if (this.props.data) {
+  //     this.createScatterplot();
+  //   }
+  // }
+  //
+  // componentDidUpdate() {
+  //   this.createScatterplot()
+  // }
   render() {
     return(
       <div>Hello Joe</div>
     )
   }
 }
+
+// function mapStateToProps({ data }) {
+//   return {}
+// }
+//
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 
 import App from './components/app';
-import reducers from './reducers';
+// import reducers from './reducers';
 require('./style.scss');
 
 const createStoreWithMiddleware = applyMiddleware(ReduxThunk)(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>,
+  // <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />,
+  // </Provider>,
   document.querySelector('#app')
 );
